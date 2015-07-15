0e6532a396320b6ce62e022855d1f456dcc6f8b6
Matti Jokitulppo <g8049@student.jamk.fi>
2015-07-15 21:40:43 +0300

    Add party alphabet!

--- src/js/partyletterz.js
+++ src/js/partyletterz.js
@@ -3,9 +3,26 @@
     var doc = window.document;
 
     var PartyLetterz = {
+        config: {
+            version: '0.0.1',
+            imgPath:  '../img/'
+        },
         partyHard: function() {
+            console.log("When it's time to party we will party hard.");
             var elements = doc.querySelectorAll('.partyletter');
-            console.log(elements);
+
+            for( var i = 0, len = elements.length; i < len; i++) {
+                var current = elements[i];
+
+                if (   current.tagName === 'P'
+                    || current.tagName === 'H1'
+                    || current.tagName === 'H2'
+                    || current.tagName === 'SPAN'
+                    ) {
+                    var text = current.childNodes[0].nodeValue;
+                    current.childNodes[0].nodeValue = "";
+                }
+            }
         }
     };
 
