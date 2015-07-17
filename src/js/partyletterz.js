b1c9fcf3cf2d18efdc23d99d221ded119f6934c8
Matti Jokitulppo <g8049@student.jamk.fi>
2015-07-17 16:44:14 +0300

    More work

--- src/js/partyletterz.js
+++ src/js/partyletterz.js
@@ -20,6 +20,23 @@
                     || current.tagName === 'SPAN'
                     ) {
                     var text = current.childNodes[0].nodeValue;
+
+                    var imageBackground = '';
+                    var imagePositions  = '';
+
+                    for (var i = 0, txtlen = text.length; i < txtlen; i++) {
+                        var char = text[i];
+                        if (char != ' ') {
+                            imageBackground +=  'url(' + "'../src/img/" + char.toUpperCase() + ".gif'" + ')';
+
+                            if (i < txtlen - 1) {
+                                imageBackground += ', ';
+                            }
+                        }
+                    }
+                    console.log(current);
+                    console.log(imageBackground);
+                    current.style['background-image'] = imageBackground;
                     current.childNodes[0].nodeValue = "";
                 }
             }
